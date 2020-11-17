7f6c63b7dfb86aca43c717eaf560f9b833d26b33
unknown <nandhinimadan20@gmail.com>
2020-11-16 22:22:27 -0800

    PUT,UPDATE And delete API completed

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -1,53 +1,75 @@
-const db=require("../database/dbconfig")
+const { update } = require("../database/dbconfig")
+const db = require("../database/dbconfig")
 
 
-async function find(){
+async function find() {
     return await db("users as u")
-    .select("u.ID","u.firstName")
+        .select("u.ID", "u.firstName")
 
 }
 
-async function addPotluck(data){
-    const {id}= await db("addPotluck").insert(data)
+async function addPotluck(potluck, Id) {
+    const data = { userId: Id, ...potluck }
+    const [id] = await db("addPotluck").insert(data).returning("potluckId")
+    console.log("PotluckId", id)
     return findById(id)
-   
+
 }
-async function addUsers(data){
-    const [id]= await db("users").insert(data).returning("ID")
-    console.log("ID",id)
+async function addUsers(data) {
+    const [id] = await db("users").insert(data).returning("ID")
+    console.log("ID", id)
     return findByUserId(id)
-   
+
 }
-async function findByUserId(id){
-    console.log("findUserBy",id)
+async function findByUserId(id) {
+    console.log("findUserBy", id)
     return await db("users")
-                .select("ID","firstName","lastName","emailId","password","userType")
-                .where("ID",id)
-               // .first()
+        .select("ID", "firstName", "lastName", "emailId", "password", "userType")
+        .where("ID", id)
+        .first()
 }
-async function findById(id){
-    console.log("addpotluck id",id)
+async function findById(id) {
+    console.log("addpotluck id", id)
     return await db("addPotluck")
-                .select("potluckId","potluckName","date","time","location","foodItems","notes","userId")
-                .where({id})
-                .first()
+        .select("potluckId", "potluckName", "date", "time", "location", "foodItems", "notes", "userId")
+        .where("potluckId", id)
+        .first()
 }
-async function findByEmailId(emailId){
-    console.log("db email",emailId)
-return await db("users")
-                .select("ID","firstName","password","emailId")
-                  .where("emailId",emailId)
-                  .first()
-
-              
-
+async function findByEmailId(emailId) {
+    console.log("db email", emailId)
+    return await db("users")
+        .select("ID", "firstName", "password", "emailId")
+        .where("emailId", emailId)
+        .first()
 }
 
-module.exports={
-    find,
-    addPotluck,
-    findById,
-    findByEmailId,
-    findByUserId,
-    addUsers,
+async function updatePotluck(data, id) {
+    console.log("updatePotluck", id)
+    await db("addPotluck")
+        .where("potluckId", id)
+        .update(data)
+    return findById(id)
+
 }
+async function deletePotluck(id){
+    console.log("delete",id)
+   return  await db("addPotluck")
+        .where("potluckId",id)
+        .del()
+}
+//-/guest/:id/invite/potLuck/:id
+// async function inviteList(guestId,potluckId,data){
+    
+//         const [id]=await db("inviteList").insert()
+// }
+
+    module.exports = {
+        find,
+        addPotluck,
+        findById,
+        findByEmailId,
+        findByUserId,
+        addUsers,
+        updatePotluck,
+        deletePotluck,
+    }
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -16,7 +16,7 @@ router.post("/login", async (req, res, next) => {
     try {
         const { emailId, password } = req.body
         const user = await model.findByEmailId(emailId)
-        console.log("user",user)
+        console.log("user", user)
         if (!user) {
             return res.status(401).json({
                 message: "Invalid Credentials"
@@ -47,35 +47,86 @@ router.post("/login", async (req, res, next) => {
 
 router.post("/register", async (req, res, next) => {
     try {
-        let { firstName, lastName, emailId, password, userType } = req.body
-        console.log("emailId",emailId)
+        const { firstName, lastName, emailId, password, userType } = req.body
+        console.log("emailId", emailId)
         const users = await model.findByEmailId(emailId)
-        console.log("users router",users)// When i get [] its working 
+        console.log("users router", users)// When i get [] its working 
 
         if (users) {
             return res.status(409).json({
                 message: "Your Email Address already registered",
             })
         }
-        else{
-            password= await bycrypt.hash(password, 14)
+        else {
+
             const newUser = await model.addUsers({
                 firstName,
                 lastName,
                 emailId,
-                password,//Password harshed 14 times
+                password: await bycrypt.hash(password, 14),//Password harshed 14 times
                 userType
             })
-            console.log("New user",newUser)
+            console.log("New user", newUser)
             res.status(201).json(newUser)
         }
-       
+
     }
     catch (err) {
         next(err)
     }
 })
+//-/:id/addPotluck
+router.post("/:id/addPotluck", async (req, res, next) => {
+    try {
+        const { potluckName, date, time, location, foodItems, notes } = req.body
+        const id = req.params.id
+        const newPotluck = await model.addPotluck(req.body, id)
+        console.log("New user", newPotluck)
+        res.status(201).json(newPotluck)
 
 
+    }
+    catch (err) {
+        next(err)
+    }
+})
+router.put("/:id/editPotluck", async (req, res, next) => {
+    try {
+        const { potluckName, date, time, location, foodItems, notes } = req.body
+        const id = req.params.id
+        const idCheck = await model.findById(id)
+        console.log("potluck", idCheck)
+        if (!idCheck) {
+            res.status(409).json({
+                message: "InValid potluck Id"
+
+            })
+
+        }
+        const editPotluck = await model.updatePotluck(req.body, id)
+        console.log("Edit", editPotluck)
+        res.status(200).json(editPotluck)
+    }
+    catch (err) {
+        next(err)
+    }
+})
+router.delete("/:id/deletePotluck",async(req,res,next)=>{
+    const id=req.params.id
+    const deleted=await model.deletePotluck(id)
+    try{
+        if(deleted){
+            res.status(200).json({
+                message:"potluck has been deleted"
+            })
+        }
+    }
+    catch(err){
+        next(err)
+    }
+    
+
+
+})
 
 module.exports = router
